709498028dc243f0ada1ed855c08f0d6a7ca48db
Esimgali <esimgalikhamitov2005@gmail.com>
2024-12-19 06:58:22 +0500

    first

---
+++ src/app/components/page.tsx
@@ -0,0 +1,9 @@
+// import { useSelector, useDispatch } from 'react-redux';
+
+export default function Counter () {
+  return (
+    <div>
+      <p>Counter: 5165160</p>
+    </div>
+  );
+};
---
+++ src/pages/api/hello.js
@@ -0,0 +1,6 @@
+// src/pages/api/hello.js
+
+export default function handler(req, res) {
+    res.status(200).json({ message: 'Hello from Next.js API!' });
+  }
+  
